8266fae483b9ab50289386f2b6b5509cce4e61b2
Connie Leung <cleung334@gmail.com>
2026-08-25 18:06:34 +0800

    chore: update footer text

--- src/app/shared/ui/layout/footer/footer.component.ts
+++ src/app/shared/ui/layout/footer/footer.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
     <footer class="app-footer">
       <div class="footer-container">
         <p class="footer-text">
-          &copy; 2026 On-device Expense Tracker. Built with Angular, Gemma 4, Litert.js &amp; Tailwind.
+          &copy; 2026 On-device Expense Tracker. Built with Angular, Gemma 4, LiteRT LM Web SDK &amp; Tailwind.
         </p>
       </div>
     </footer>
